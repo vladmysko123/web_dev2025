35d021993406f6b7586951310c0da2ec89e84909
vladmysko123 <113307395+vladmysko123@users.noreply.github.com>
2025-04-09 12:17:32 +0200

    Create creational_pattern.py

---
+++ first_lab/creational_pattern.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+class AnimalFactory:
+    @staticmethod
+    def create_animal(animal_type):
+        if animal_type == "dog":
+            return Dog()
+        elif animal_type == "cat":
+            return Cat()
+        else:
+            raise ValueError("Unknown animal type")
+
+animal1 = AnimalFactory.create_animal("dog")
+animal2 = AnimalFactory.create_animal("cat")
+
+print(animal1.speak())  
+print(animal2.speak())  
